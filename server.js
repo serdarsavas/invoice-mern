cda02ba5ced2462594acb3d87385fdebeb4a7afa
cerdarius <serdar.savas84@gmail.com>
2020-05-02 20:14:57 +0200

    First setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+//TODO Define routes
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
