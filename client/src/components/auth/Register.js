ee8d0e2a951210077dfa1565ce0f709b2b5adf60
cerdarius <serdar.savas84@gmail.com>
2020-05-03 23:05:36 +0200

    Create auth flow

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,102 @@
+import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
+import PropTypes from 'prop-types';
+
+const Register = ({ setAlert, register, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    if (password !== password2) {
+      return setAlert('Lösenorden matchar inte', 'danger');
+    }
+
+    register({ name, email, password });
+  };
+
+  // if (isAuthenticated) {
+  //   return <Redirect to='/dashboard' />;
+  // }
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Skapa ett konto
+      </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Epostadress'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Lösenord'
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Bekräfta lösenord'
+            name='password2'
+            minLength='6'
+            value={password2}
+            onChange={e => onChange(e)}
+          />
+        </div>
+        <input
+          type='submit'
+          className='btn btn-primary'
+          value='Registrera dig'
+        />
+      </form>
+      <p className='my-1'>
+        Already have an account? <Link to='/login'>Logga in</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => {
+  return { isAuthenticated: state.auth.isAuthenticated };
+};
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <div style={{ width: '200px', margin: 'auto' }}>
+    <i className="fa fa-spinner fa-spin fa-4x"></i>
+  </div>
+);
